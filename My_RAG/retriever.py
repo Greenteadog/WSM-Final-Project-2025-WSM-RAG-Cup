a959840b6dfe579441af5b5b6e186473299b535a
kt-cheng <katy881115@gmail.com>
2025-12-03 18:27:21 +0800

    add stemmer for English retrieval

--- My_RAG/retriever.py
+++ My_RAG/retriever.py
@@ -1,22 +1,33 @@
 from rank_bm25 import BM25Okapi
 import jieba
+from nltk.stem import PorterStemmer
 
 class BM25Retriever:
     def __init__(self, chunks, language="en"):
         self.chunks = chunks
         self.language = language
         self.corpus = [chunk['page_content'] for chunk in chunks]
+        
+        self.stemmer = PorterStemmer()
+        
         if language == "zh":
             self.tokenized_corpus = [list(jieba.cut(doc)) for doc in self.corpus]
         else:
-            self.tokenized_corpus = [doc.split(" ") for doc in self.corpus]
+            self.tokenized_corpus = []
+            for doc in self.corpus:
+                tokens = doc.lower().split()
+                stemmed_tokens = [self.stemmer.stem(token) for token in tokens]
+                self.tokenized_corpus.append(stemmed_tokens)
+        
         self.bm25 = BM25Okapi(self.tokenized_corpus)
 
     def retrieve(self, query, top_k=5):
         if self.language == "zh":
             tokenized_query = list(jieba.cut(query))
         else:
-            tokenized_query = query.split(" ")
+            tokens = query.lower().split()
+            tokenized_query = [self.stemmer.stem(token) for token in tokens]
+
         top_chunks = self.bm25.get_top_n(tokenized_query, self.chunks, n=top_k)
         return top_chunks
 
